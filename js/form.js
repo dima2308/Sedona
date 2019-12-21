6d5bb396a9816a5ea41683b7902fc7b5fbf25b70
Dmitry <dimabatarev@mail.ru>
2019-12-21 14:30:39 +0400

    Провёл структуризацию файлов

--- js/form.js
+++ js/form.js
@@ -1,20 +1,16 @@
+var link = document.querySelector(".btn-search"); // поиск элемента по селектору
+var popup = document.querySelector(".modal-content");
+var close = popup.querySelector(".modal-content-close");
 
-			var link = document.querySelector(".btn-search"); // поиск элемента по селектору
-			var popup = document.querySelector(".modal-content");
-			var close = popup.querySelector(".modal-content-close");
-
+link.addEventListener("click", function(event) { // обработка события для переменной
+	event.preventDefault(); // убирает действие по умолчанию 
+	popup.classList.add("modal-content-show"); // добавляет класс к элементу
+});
 			
-			link.addEventListener("click", function(event) { // обработка события для переменной
-				event.preventDefault(); // убирает действие по умолчанию 
-				popup.classList.add("modal-content-show"); // добавляет класс к элементу
-							
-			});
-			
-			window.addEventListener("keydown", function(event) {
-				if (event.keyCode == 27) {
-					if (popup.classList.contains("modal-content-show")) {
-						popup.classList.remove("modal-content-show");	
-						}
-					}
-			});
-			
+window.addEventListener("keydown", function(event) {
+	if (event.keyCode == 27) {
+		if (popup.classList.contains("modal-content-show")) {
+			popup.classList.remove("modal-content-show");	
+		}
+	}
+});
