dfadab0cdd748bfba0a9da36d1328fb397a9852b
Dmitry <dimabatarev@mail.ru>
2020-10-03 13:55:02 +0400

    Правки в оформлении

--- js/form.js
+++ js/form.js
@@ -2,15 +2,15 @@ var link = document.querySelector(".btn-search"); // поиск элемента
 var popup = document.querySelector(".modal-content");
 var close = popup.querySelector(".modal-content-close");
 
-link.addEventListener("click", function(event) { // обработка события для переменной
+link.addEventListener("click", function (event) { // обработка события для переменной
 	event.preventDefault(); // убирает действие по умолчанию 
 	popup.classList.add("modal-content-show"); // добавляет класс к элементу
 });
-			
-window.addEventListener("keydown", function(event) {
+
+window.addEventListener("keydown", function (event) {
 	if (event.keyCode == 27) {
 		if (popup.classList.contains("modal-content-show")) {
-			popup.classList.remove("modal-content-show");	
+			popup.classList.remove("modal-content-show");
 		}
 	}
 });
